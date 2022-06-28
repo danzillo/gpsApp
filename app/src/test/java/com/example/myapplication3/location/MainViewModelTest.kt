27d4c137e57b965137fcc2ce83e6912e1603d48c
danzillo <dan4ikhom@gmail.com>
2022-06-28 10:40:09 +0700

    Проведено тестирование определения расстояний библиотекой geoLib

--- app/src/test/java/com/example/myapplication3/location/MainViewModelTest.kt
+++ app/src/test/java/com/example/myapplication3/location/MainViewModelTest.kt
@@ -3,12 +3,13 @@ package com.example.myapplication3.location
 import android.app.Application
 import org.junit.Assert
 import org.junit.Test
-
+import net.sf.geographiclib.*
 
 internal class MainViewModelTest {
 
     private val viewModelTest = MainViewModel(application = Application())
 
+    // Тестирование метода определения координат для точек антиподов
     @Test
     fun lengthBetweenCoordinate() {
         var lengthOfRoad: Double = 0.0
@@ -24,7 +25,25 @@ internal class MainViewModelTest {
                 )
             counter += 1
         }
-        Assert.assertEquals(4, lengthOfRoad)
+        Assert.assertEquals(3625.184, lengthOfRoad, 0.01)
     }
 
+    // Тестирование с помощью библиотеки
+    @Test
+    fun geoLibLength() {
+        var lengthOfRoad: Double = 0.0
+        val lastCoordinateIndex = axis.lastIndex
+        var counter = 0
+        while (counter < lastCoordinateIndex) {
+            lengthOfRoad +=
+                Geodesic.WGS84.Inverse(
+                    axis[counter].getLat(),
+                    axis[counter].getLong(),
+                    axis[counter + 1].getLat(),
+                    axis[counter + 1].getLong()
+                ).s12
+            counter += 1
+        }
+        Assert.assertEquals(3625.184, lengthOfRoad, 0.01)
+    }
 }
