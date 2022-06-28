1cc76e0617df32e5f90dcaa23ea6dcacee0133fd
danzillo <dan4ikhom@gmail.com>
2022-06-28 14:49:26 +0700

    Добавлено тестирование поиска расстояния до столба

---
+++ app/src/test/java/com/example/myapplication3/location/GeoLibTest.kt
@@ -0,0 +1,103 @@
+package com.example.myapplication3.location
+
+import org.junit.Test
+import net.sf.geographiclib.*
+import org.junit.Assert.assertEquals
+import kotlin.math.cos
+
+internal class GeoLibTest {
+
+    // Тестирование с помощью библиотеки
+    @Test
+    fun geoLibLength() {
+        var lengthOfRoad: Double = 0.0
+        val lastCoordinateIndex = axis.lastIndex
+        var counter = 0
+        while (counter < lastCoordinateIndex) {
+            lengthOfRoad +=
+                Geodesic.WGS84.Inverse(
+                    axis[counter].latitude,
+                    axis[counter].longitude,
+                    axis[counter + 1].latitude,
+                    axis[counter + 1].longitude
+                ).s12
+            counter += 1
+        }
+        assertEquals(3625.184, lengthOfRoad, 0.01)
+    }
+
+    @Test
+    fun geoLibCrossPoint() {
+        var lengthToColumn: Double
+        var minLengthToColumn: Double = Double.MAX_VALUE
+
+        var totalRoadLength = 0.0
+        var lengthOfRoadToColumnVertex = 0.0
+        var nextSectionRoadLength = 0.0
+        var previousSectionRoadLength = 0.0
+
+        var projection = 0.0
+
+        val lastCoordinateIndex = axis.lastIndex
+
+        // Проходимся по всем вершинам
+        for (counter in 0 until lastCoordinateIndex - 1) {
+
+            // Находим расстояние от каждой вершины до столба
+            lengthToColumn =
+                Geodesic.WGS84.Inverse(
+                    axis[counter].latitude,
+                    axis[counter].longitude,
+                    distanceMarks[0].latitude,
+                    distanceMarks[0].longitude
+                ).s12
+
+            // Считаем длину суммарную длину дороги между вершинами
+            totalRoadLength += Geodesic.WGS84.Inverse(
+                axis[counter].latitude,
+                axis[counter].longitude,
+                axis[counter + 1].latitude,
+                axis[counter + 1].longitude
+            ).s12
+
+            /* Если найденное расстояние меньше того, что было, то сохраняем его с длиной
+                участков дороги (от данной вершины до следующей и от данной вершины до предыдущей) */
+            if (lengthToColumn < minLengthToColumn) {
+                minLengthToColumn = lengthToColumn
+                nextSectionRoadLength = Geodesic.WGS84.Inverse(
+                    axis[counter].latitude,
+                    axis[counter].longitude,
+                    axis[counter + 1].latitude,
+                    axis[counter + 1].longitude
+                ).s12
+
+                if (counter > 0)
+                    previousSectionRoadLength = Geodesic.WGS84.Inverse(
+                        axis[counter].latitude,
+                        axis[counter].longitude,
+                        axis[counter - 1].latitude,
+                        axis[counter - 1].longitude
+                    ).s12
+
+                // Записываем длину дороги до вершины около столба
+                lengthOfRoadToColumnVertex = totalRoadLength - nextSectionRoadLength
+            }
+        }
+
+        /* Зная длину до столба, длину участка дороги между точками
+           можно найти cos.
+           косинус * | длДоСтолба | = длине проекции */
+        projection =
+            if (minLengthToColumn * cos(nextSectionRoadLength / minLengthToColumn) > 0.0
+                && minLengthToColumn * cos(previousSectionRoadLength / minLengthToColumn) != 0.0
+            )
+                minLengthToColumn * cos(nextSectionRoadLength / minLengthToColumn)
+            else minLengthToColumn * cos(previousSectionRoadLength / minLengthToColumn)
+
+        println("lengthOfRoad = $lengthOfRoadToColumnVertex")
+        println("projection = $projection")
+        println("(projection + lengthOfRoad) = ${projection + lengthOfRoadToColumnVertex}")
+
+        assertEquals(90, projection + lengthOfRoadToColumnVertex)
+    }
+}
--- app/src/test/java/com/example/myapplication3/location/MainViewModelTest.kt
+++ app/src/test/java/com/example/myapplication3/location/MainViewModelTest.kt
@@ -3,9 +3,6 @@ package com.example.myapplication3.location
 import android.app.Application
 import org.junit.Assert
 import org.junit.Test
-import net.sf.geographiclib.*
-import org.junit.Assert.assertEquals
-import kotlin.math.cos
 
 internal class MainViewModelTest {
 
@@ -29,88 +26,4 @@ internal class MainViewModelTest {
         }
         Assert.assertEquals(3625.184, lengthOfRoad, 0.01)
     }
-
-    // Тестирование с помощью библиотеки
-    @Test
-    fun geoLibLength() {
-        var lengthOfRoad: Double = 0.0
-        val lastCoordinateIndex = axis.lastIndex
-        var counter = 0
-        while (counter < lastCoordinateIndex) {
-            lengthOfRoad +=
-                Geodesic.WGS84.Inverse(
-                    axis[counter].latitude,
-                    axis[counter].longitude,
-                    axis[counter + 1].latitude,
-                    axis[counter + 1].longitude
-                ).s12
-            counter += 1
-        }
-        assertEquals(3625.184, lengthOfRoad, 0.01)
-    }
-
-
-    @Test
-    fun geoLibCrossPoint() {
-        var lengthToStolb: Double = Double.MAX_VALUE
-        var lengthOfRoad: Double = 0.0
-        var minLengthToStolb: Double = Double.MAX_VALUE
-        val lastCoordinateIndex = axis.lastIndex
-        var counter = 0
-        var counter2 = 0
-        var saveCounter = 0
-
-        // Проходимся по всем вершинам
-        while (counter < lastCoordinateIndex) {
-
-            // Находим расстояние от вершины до столба
-            lengthToStolb =
-                Geodesic.WGS84.Inverse(
-                    axis[counter].latitude,
-                    axis[counter].longitude,
-                    distanceMarks[0].latitude,
-                    distanceMarks[0].longitude
-                ).s12
-
-            // Если найденное расстояние меньше того, что было, то сохраняем его
-            // и длину дороги между точками
-            if (lengthToStolb < minLengthToStolb) {
-                minLengthToStolb = lengthToStolb
-                lengthOfRoad = Geodesic.WGS84.Inverse(
-                    axis[counter].latitude,
-                    axis[counter].longitude,
-                    axis[counter + 1].latitude,
-                    axis[counter + 1].longitude
-                ).s12
-                // Сохраняем счетчик, чтобы потом найти общую длину дороги до столба
-                saveCounter = counter
-            }
-
-            counter += 1
-        }
-
-        /* Зная длину до столба, длину участка дороги между точками
-           можно найти cos, косинус * | длДоСтолба | = длине проекции длДоСтолба
-           на длину дороги*/
-        val projection = minLengthToStolb * cos(lengthOfRoad / minLengthToStolb)
-        lengthOfRoad = 0.0
-
-        while (counter2 < saveCounter) {
-            lengthOfRoad +=
-                Geodesic.WGS84.Inverse(
-                    axis[counter2].latitude,
-                    axis[counter2].longitude,
-                    axis[counter2 + 1].latitude,
-                    axis[counter2 + 1].longitude
-                ).s12
-            counter2 += 1
-        }
-
-        println("lengthOfRoad = $lengthOfRoad")
-        println("projection = $projection")
-        println("(projection + lengthOfRoad) = ${projection + lengthOfRoad}")
-
-        assertEquals(90, projection + lengthOfRoad)
-    }
-
 }
